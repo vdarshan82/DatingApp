17af8e627f17b61aeb066ed12ef658d235e06833
Darshan Vyas <vdarshan82@gmail.com>
2020-03-31 18:41:25 +0530

    Initial Commit

---
+++ DatingApp-SPA/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ValueComponent
+   ],
+   imports: [
+      HttpClientModule,
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
